d4a5fe2a3a4d6cd204e6762fe8c709b627d7768a
rushils372 <59312459+rushils372@users.noreply.github.com>
2021-12-02 23:23:57 +0530

    Note Wise json done

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,22 +1,110 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import { ButtonGroup, Container, FormControlLabel, RadioGroup, FormControl, FormLabel } from '@material-ui/core'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core';
+import { TextField, Radio } from '@material-ui/core';
+import { useHistory } from 'react-router';
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
+})
 
 export default function Create() {
+
+  const classes = useStyles()
+  const history = useHistory()
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+  const [category, setCategory] = useState('todos')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTitleError(false)
+    setDetailsError(false)
+
+    if (title == '') {
+      setTitleError(true)
+    }
+
+    if (details == '') {
+      setDetailsError(true)
+    }
+
+    if (title && details) {
+      fetch('http://localhost:8000/notes', {
+        method: 'POST',
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify({ title, details, category })
+      }).then(() => history.push('/') )
+    }
+  }
+
   return (
-    <div>
-      {/* <Typography
-        variant="h1"
-        color="primary"
-        align="center"
-      >
-        Create a new Note
-      </Typography> */}
+    <Container>
       <Typography
-        noWrap
-        color="secondary"
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
       >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis impedit placeat, dolores vero excepturi consequatur itaque provident hic eligendi, eveniet neque quisquam modi alias libero. Fuga ipsam nihil aliquid? Aliquam inventore doloribus laborum provident accusantium temporibus corporis quis dignissimos eum repellat cumque, eveniet rem.
+        Create a New Note
       </Typography>
-    </div>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+        
+        <FormControl className={classes.field}>
+        <FormLabel>Note Category</FormLabel>
+        <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
+          <FormControlLabel value="money" control={<Radio />} label="Money"/>
+          <FormControlLabel value="todos" control={<Radio />} label="Todos"/>
+          <FormControlLabel value="remainders" control={<Radio />} label="Remainders"/>
+          <FormControlLabel value="work" control={<Radio />} label="Work"/>
+        </RadioGroup>
+        </FormControl>
+
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+
+      </form>
+
+
+
+    </Container>
   )
 }
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,22 @@
-import React from 'react'
+// json-server --watch data/db.json --port 8000
+
+import React, { useEffect, useState } from 'react'
 
 export default function Notes() {
+
+  const[notes, setNotes] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8000/notes')
+      .then(res => res.json())
+      .then(data => setNotes(data))
+  }, [])
+
   return (
     <div>
-      Notes page
+      {notes.map(note => (
+        <p key={notes.id}>{ note.title }</p>
+      ))}
     </div>
   )
 }

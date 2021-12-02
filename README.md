935801c3f4c12eb2e52bde87a6d49ed2bc568719
rushils372 <59312459+rushils372@users.noreply.github.com>
2021-12-02 20:00:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### This is a Simple Note taking app based on React JS and Material UI
+
+Download the code & run **npm install** or **npm i** to install dependencies before starting the app.
+
---
+++ src/pages/Create.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+export default function Create() {
+  return (
+    <div>
+      {/* <Typography
+        variant="h1"
+        color="primary"
+        align="center"
+      >
+        Create a new Note
+      </Typography> */}
+      <Typography
+        noWrap
+        color="secondary"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis impedit placeat, dolores vero excepturi consequatur itaque provident hic eligendi, eveniet neque quisquam modi alias libero. Fuga ipsam nihil aliquid? Aliquam inventore doloribus laborum provident accusantium temporibus corporis quis dignissimos eum repellat cumque, eveniet rem.
+      </Typography>
+    </div>
+  )
+}
